79343a96c38f4c9d275805aa24255c05b202c3f8
Santiago Pereda <santiago.pereda1@gmail.com>
2023-05-22 11:54:10 +0200

    update: git alias

--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -8,7 +8,7 @@ alias gits='git status'
 alias gita='git add'
 alias gitc='git commit -m'
 alias gitp='git push origin main'
-alias exclude='update-index --skip-worktree'
+alias exclude='!sh -c "echo "$1" >> .git/info/exclude" -'
 
 # system
 alias explorer='explorer.exe'
