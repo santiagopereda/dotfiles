3adf57cbc8983b025379d739c13985e1c6a1ce32
Santiago Pereda <santiago.pereda1@gmail.com>
2024-05-18 02:17:40 +0200

    Update: tmux config

--- tmux/ssh_active.sh
+++ tmux/ssh_active.sh
@@ -3,7 +3,7 @@
 # Check if an SSH connection is active
 if tmux list-panes -t "$TMUX_PANE" | grep "SSH" >/dev/null 2>&1; then
   # SSH connection is active
-  echo 
+  echo " "
 else
   # SSH connection is inactive
   echo 
